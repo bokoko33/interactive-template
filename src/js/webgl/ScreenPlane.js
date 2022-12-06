562be80863ae5b960da9720af9f170b60d1d76ca
bokoko33 <work.bokoko33@gmail.com>
2022-12-06 12:50:47 +0900

    複数のtexture load に対応

--- src/js/webgl/ScreenPlane.js
+++ src/js/webgl/ScreenPlane.js
@@ -1,22 +1,20 @@
 import * as THREE from 'three';
 import vertexShader from '~/glsl/plane.vert';
 import fragmentShader from '~/glsl/plane.frag';
+import { loadMultipleTexture } from '~/js/webgl/multipleTextureLoader';
 
 /**
  * canvas全体を覆うPlane Mesh
  */
 export class ScreenPlane {
   constructor({ canvasSize }) {
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('/images/sample1.jpg');
-
     const geometry = new THREE.PlaneGeometry(2, 2);
     const material = new THREE.RawShaderMaterial({
       uniforms: {
         uTime: { value: 0 },
         uMouse: { value: new THREE.Vector2(0, 0) },
         uResolution: { value: new THREE.Vector2(canvasSize.w, canvasSize.h) },
-        uTexture: { value: texture },
+        uTexture: { value: null },
       },
       vertexShader,
       fragmentShader,
@@ -24,6 +22,11 @@ export class ScreenPlane {
     });
 
     this.mesh = new THREE.Mesh(geometry, material);
+
+    const texturePaths = ['/images/sample1.jpg', '/images/sample2.jpg'];
+    const textures = loadMultipleTexture(texturePaths).then((textures) => {
+      this.mesh.material.uniforms.uTexture.value = textures[1];
+    });
   }
 
   update = ({ time, mouse }) => {
