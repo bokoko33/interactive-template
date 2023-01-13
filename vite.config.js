ca41534bf111bdfb6d54e1573ae74b7916da33bf
bokoko33 <work.bokoko33@gmail.com>
2023-01-13 17:24:41 +0900

    remove unused comment

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // --- ----
 
-// ref: https://zenn.dev/sakata_kazuma/articles/59a741489c8bbc
 export default defineConfig({
   server: {
     host: true,
