062e9e10db95c4228b7b34ec9b47495b147a04a0
bokoko33 <work.bokoko33@gmail.com>
2023-03-02 00:53:21 +0900

    AssetLoaderをelementにも対応

--- src/_lib/webgl/AssetLoader.js
+++ src/_lib/webgl/AssetLoader.js
@@ -1,44 +1,95 @@
 import { EventEmitter } from 'events';
-import * as THREE from 'three';
+import { VideoTexture } from 'three/src/textures/VideoTexture';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
-const loader = new THREE.TextureLoader();
-const loadTexture = async (path) => {
+const loader = new TextureLoader();
+
+const loadByPathString = (path, resolve) => {
   const fileType = path.split('.').pop();
   const isVideo = ['mp4', 'webm'].includes(fileType);
 
-  return new Promise((resolve) => {
-    if (isVideo) {
-      // --- 動画の場合 ---
-      const videoEl = document.createElement('video');
-      const videoTexture = new THREE.VideoTexture(videoEl);
-      videoEl.oncanplay = () => {
-        // width/heightの取り方がvideo/imageで異なってややこしいのでまとめる
-        resolve({
-          data: videoTexture,
-          width: videoTexture.image.videoWidth,
-          height: videoTexture.image.videoHeight,
-        });
-      };
+  if (isVideo) {
+    // --- 動画の場合 ---
+    const videoEl = document.createElement('video');
+    const videoTexture = new VideoTexture(videoEl);
+    videoEl.onloadeddata = () => {
+      resolve({
+        data: videoTexture,
+        width: videoTexture.image.videoWidth,
+        height: videoTexture.image.videoHeight,
+      });
+    };
 
-      videoEl.src = path;
-      videoEl.crossOrigin = 'Anonymous';
-      videoEl.loop = true;
-      videoEl.muted = true;
-      videoEl.playsInline = true;
-      videoEl.play(); // ここでもう再生しておいて、使う側で再生位置をいじる
-    } else {
-      // --- 画像の場合 ---
-      loader.load(path, (res) =>
-        resolve({
-          data: res,
-          width: res.image.width,
-          height: res.image.height,
-        })
-      );
-    }
-  });
+    videoEl.src = path;
+    videoEl.crossOrigin = 'Anonymous';
+    videoEl.loop = true;
+    videoEl.muted = true;
+    videoEl.playsInline = true;
+    videoEl.play(); // ここでもう再生しておいて、使う側で再生位置をいじる
+  } else {
+    // --- 画像の場合 ---
+    loader.load(path, (res) =>
+      resolve({
+        data: res,
+        width: res.image.width,
+        height: res.image.height,
+      })
+    );
+  }
 };
 
+const loadByHTMLElement = (el, resolve) => {
+  const isVideo = el.tagName === 'VIDEO';
+
+  if (isVideo) {
+    // --- 動画の場合 ---
+    const videoTexture = new VideoTexture(el);
+    el.onloadeddata = () => {
+      el.play(); // ここでもう再生しておく
+      resolve({
+        data: videoTexture,
+        width: videoTexture.image.videoWidth,
+        height: videoTexture.image.videoHeight,
+      });
+    };
+
+    // onloadeddataがすでに発火している可能性があるのでsrcをリフレッシュ
+    const src = el.src;
+    el.src = '';
+    el.src = src;
+  } else {
+    // --- 画像の場合 ---
+    loader.load(el.src, (res) =>
+      resolve({
+        data: res,
+        width: res.image.width,
+        height: res.image.height,
+      })
+    );
+  }
+};
+
+const loadTexture = async (prop) => {
+  const promise = new Promise((resolve, reject) => {
+    if (typeof prop === 'string') {
+      loadByPathString(prop, resolve);
+    } else if (prop instanceof HTMLElement) {
+      loadByHTMLElement(prop, resolve);
+    } else {
+      reject();
+    }
+  });
+
+  return promise;
+};
+
+/**
+ * @param {string | HTMLElement} prop
+ *
+ * width/heightの取り方がvideo/imageで異なってややこしいので、
+ * {data: Texture, width: number, height: number}
+ * のオブジェクトで返す。複数ならそれを配列に。
+ */
 export class AssetLoader extends EventEmitter {
   constructor() {
     super();
